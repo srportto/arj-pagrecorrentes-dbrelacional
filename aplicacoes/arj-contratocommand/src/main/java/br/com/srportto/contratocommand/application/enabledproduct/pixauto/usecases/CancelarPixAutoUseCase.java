7f1b35ff84fa05d6a91086e02fe6fa2e703ca082
srportto <ceeh1100@gmail.com>
2026-06-13 16:24:58 -0300

    ajusta status persistido no cancelamento

--- aplicacoes/arj-contratocommand/src/main/java/br/com/srportto/contratocommand/application/enabledproduct/pixauto/usecases/CancelarPixAutoUseCase.java
+++ aplicacoes/arj-contratocommand/src/main/java/br/com/srportto/contratocommand/application/enabledproduct/pixauto/usecases/CancelarPixAutoUseCase.java
@@ -1,6 +1,15 @@
 package br.com.srportto.contratocommand.application.enabledproduct.pixauto.usecases;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContextException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import br.com.srportto.contratocommand.application.defaultservice.cancelamento.CancelamentoValidator;
 import br.com.srportto.contratocommand.application.enabledproduct.pixauto.PixAutoRepository;
 import br.com.srportto.contratocommand.domain.entities.Autorizacao;
@@ -11,14 +20,6 @@ import br.com.srportto.contratocommand.entrypoint.contratosrest.AutorizacaoCompl
 import br.com.srportto.contratocommand.entrypoint.contratosrest.CancelarAutorizacaoRequestDto;
 import br.com.srportto.contratocommand.shared.exceptions.BusinessException;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContextException;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Component
 @AllArgsConstructor
@@ -43,7 +44,7 @@ public class CancelarPixAutoUseCase {
 
         cancelamentoValidator.validar(request);
 
-        autorizacao.setStatus(3); // cancelada
+        autorizacao.setStatus(5); // cancelada
         var dadosCancelamento = new Cancelamento();
 
         var dataHoraCancelamento = LocalDateTime.now();
